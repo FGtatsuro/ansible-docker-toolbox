2e2a32d711a89fcf1031a0b5194b7cb7fe41344f
FGtatsuro <fujiistorage@gmail.com>
2016-07-27 11:10:31 +0900

    Add a spec about Docker option.

--- spec/docker_toolbox_spec.rb
+++ spec/docker_toolbox_spec.rb
@@ -16,3 +16,11 @@ end
 describe command('which docker-compose') do
   its(:exit_status) { should eq 0 }
 end
+
+describe file('/etc/systemd/system/docker.service.d/docker.conf'), :if => os[:family] == 'debian' do
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'root' }
+  its(:content) { should include('[Service]') }
+  its(:content) { should include('ExecStart=\n') }
+  its(:content) { should include('ExecStart=/usr/bin/docker daemon -H fd:// --insecure-registry 192.168.1.1 --insecure-registry 192.168.1.2') }
+end
