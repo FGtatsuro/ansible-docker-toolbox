279c522d3d6fc9182a2c8a5cef1a002379051ee6
FGtatsuro <fujiistorage@gmail.com>
2016-07-27 18:00:17 +0900

    Add port number to Examples of insecure registry option.

--- README.md
+++ README.md
@@ -30,7 +30,7 @@ The variables we can use in this role.
 |docker_compose_download_url|Download URL for Docker Compose binary. Different architecture/version has different URL.|https://github.com/docker/compose/releases/download/1.6.2/docker-compose-Linux-x86_64|
 |docker_compose_sha256|SHA256 signature of Docker Compose binary. This is used for idempotency.|7c453a3e52fb97bba34cf404f7f7e7913c86e2322d612e00c71bd1588587c91e|
 |docker_compose_bin_path|Path Docker Compose binary is put.|/usr/local/bin/docker-compose|
-|docker_daemon_options|Options Docker daemon uses. It's defined as string like `-g /mnt/docker --insecure-registry 192.168.1.1`.|-H fd://|
+|docker_daemon_options|Options Docker daemon uses. It's defined as string like `-g /mnt/docker --insecure-registry 192.168.1.1:5000`.|-H fd://|
 
 - These variables are valid only on Debian/Ubuntu, and they aren't used on OSX.
 - If you want to overwrite values, please check following sites.
--- spec/docker_toolbox_spec.rb
+++ spec/docker_toolbox_spec.rb
@@ -29,12 +29,12 @@ describe file('/etc/systemd/system/docker.service.d/docker.conf'), :if => ['debi
   it { should be_grouped_into 'root' }
   its(:content) { should include('[Service]') }
   its(:content) { should include("ExecStart=\n") }
-  its(:content) { should include('ExecStart=/usr/bin/docker daemon -H fd:// --insecure-registry 192.168.1.1 --insecure-registry 192.168.1.2') }
+  its(:content) { should include('ExecStart=/usr/bin/docker daemon -H fd:// --insecure-registry 192.168.1.1:5000 --insecure-registry 192.168.1.2:5000') }
 end
 
 describe file('/etc/default/docker'), :if => ['debian', 'ubuntu'].include?(os[:family]) do
   it { should be_file }
   it { should be_owned_by 'root' }
   it { should be_grouped_into 'root' }
-  its(:content) { should include('DOCKER_OPTS="-H fd:// --insecure-registry 192.168.1.1 --insecure-registry 192.168.1.2"') }
+  its(:content) { should include('DOCKER_OPTS="-H fd:// --insecure-registry 192.168.1.1:5000 --insecure-registry 192.168.1.2:5000"') }
 end
