e499743c23b680814102646894713d30648c352e
FGtatsuro <fujiistorage@gmail.com>
2016-07-27 15:26:13 +0900

    Add a spec helper for Vagrant VM.

--- spec/docker_toolbox_spec.rb
+++ spec/docker_toolbox_spec.rb
@@ -17,10 +17,24 @@ describe command('which docker-compose') do
   its(:exit_status) { should eq 0 }
 end
 
-describe file('/etc/systemd/system/docker.service.d/docker.conf'), :if => os[:family] == 'debian' do
+describe file('/etc/systemd/system/docker.service.d'), :if => ['debian', 'ubuntu'].include?(os[:family]) do
+  it { should be_directory }
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'root' }
+end
+
+describe file('/etc/systemd/system/docker.service.d/docker.conf'), :if => ['debian', 'ubuntu'].include?(os[:family]) do
+  it { should be_file }
   it { should be_owned_by 'root' }
   it { should be_grouped_into 'root' }
   its(:content) { should include('[Service]') }
-  its(:content) { should include('ExecStart=\n') }
+  its(:content) { should include("ExecStart=\n") }
   its(:content) { should include('ExecStart=/usr/bin/docker daemon -H fd:// --insecure-registry 192.168.1.1 --insecure-registry 192.168.1.2') }
+end
+
+describe file('/etc/default/docker'), :if => ['debian', 'ubuntu'].include?(os[:family]) do
+  it { should be_file }
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'root' }
+  its(:content) { should include('DOCKER_OPTS="-H fd:// --insecure-registry 192.168.1.1 --insecure-registry 192.168.1.2"') }
 end
